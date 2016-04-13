cac777408590f683a7391cf00aa76d705159a36a
Voronetskyy Yevgen <yvoronetskyy@heliostech.hk>
2016-04-13 19:14:09 +0300

    refactor google maps script

--- js/script.js
+++ js/script.js
@@ -191,61 +191,6 @@ $(document).ready(function() {
   });
   });
 
- //Google Map
- $(document).ready(function() {
-
-  "use strict";
-
-  var latitude = $('#google-map').data('latitude')
-  var longitude = $('#google-map').data('longitude')
-  function initialize_map() {
-    var myLatlng = new google.maps.LatLng(latitude,longitude);
-    var mapOptions = {
-      zoom: 17,
-      scrollwheel: false,
-      center: myLatlng
-    };
-    var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-    var contentString = '';
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map
-    });
-
-  }
-  google.maps.event.addDomListener(window, 'load', initialize_map);
-
-    //Show and hide map
-    function showMap(){
-      $('.opened').fadeIn();
-      $('.closed').fadeOut();
-      $('#map-canvas').animate({height:'500px'});
-      $('#map-canvas').css('overflow','visible');
-
-      var offsetTop = $('#map-canvas').offset().top;
-      $('html, body').animate({scrollTop: offsetTop},500);
-    };
-    function hideMap(){
-      $('.opened').fadeOut();
-      $('.closed').fadeIn();
-      $('#map-canvas').animate({height:'50px'},1000);
-      $('#map-canvas').css('overflow','hidden');
-    }
-    
-    var closed=true;
-    $('.map-head').click(
-      function(){
-        if(closed){
-          showMap(); 
-          return closed=false;
-        } else if(!closed){
-          hideMap();
-          return closed=true;
-        }
-      }
-      );    
-  });
-
     //Skills charts
 
     $(document).ready(function() {
